869496ea4871d424a47e320486af10e489e8b396
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-03-20 10:44:52 +0530

    Initial Project Add

---
+++ src/main/java/com/ajayonjava/aojpetclinic/AojPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ajayonjava.aojpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AojPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AojPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajayonjava/aojpetclinic/AojPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajayonjava.aojpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AojPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
