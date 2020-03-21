7e95ffb46f3417710370a41b8a9f1566720b6712
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-03-21 10:04:18 +0530

    Refactoring Multi Module Maven Project. Closes #1 Resolved maven repackage issue

--- pet-clinic-web/src/test/java/aojpetclinic/AojPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/aojpetclinic/AojPetClinicApplicationTests.java
@@ -1,9 +1,10 @@
 package aojpetclinic;
 
+import com.ajayonjava.aojpetclinic.AojPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {AojPetClinicApplication.class})
 class AojPetClinicApplicationTests {
 
     @Test
