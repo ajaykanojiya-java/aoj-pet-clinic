77327e7cfa6102e0912685f2ce21b3159f4c283d
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-03-25 10:55:27 +0530

    Create Vet Index Page and Controllers. Closes #7

---
+++ pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.ajayonjava.aojpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listIndex(){
+        return "vets/index";
+    }
+}
