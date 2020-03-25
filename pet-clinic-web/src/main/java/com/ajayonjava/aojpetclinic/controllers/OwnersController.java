66f3f908c8545ce4e3379fd3cb4ec319c8933ed7
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-03-25 11:04:33 +0530

    Create Owner Index Page and Controller. Closes #13

---
+++ pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/controllers/OwnersController.java
@@ -0,0 +1,14 @@
+package com.ajayonjava.aojpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String getOwnersList(){
+        return "owners/index";
+    }
+}
