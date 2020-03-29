2f5ba3982c5d31bc43421b1ce50e1c580b7bc45d
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-03-29 12:29:21 +0530

    List All Vets on Vet Index Page. Closes #15

--- pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.ajayonjava.aojpetclinic.controllers;
 
+import com.ajayonjava.aojpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listIndex(){
+    public String listIndex(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
