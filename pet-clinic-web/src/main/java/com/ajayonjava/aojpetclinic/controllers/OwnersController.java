8acc3d65569cfd66ca10fb74ba070ef1a3d7861b
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-03-29 12:20:44 +0530

    List All Owners on Owners Index Page. Closes #17

--- pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/controllers/OwnersController.java
@@ -1,14 +1,23 @@
 package com.ajayonjava.aojpetclinic.controllers;
 
+import com.ajayonjava.aojpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
+    OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String getOwnersList(){
+    public String getOwnersList(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
