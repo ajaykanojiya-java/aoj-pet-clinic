c43b20b45dd5b11bd91b71d4b273c738303643f3
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-04-12 19:13:30 +0530

    Create Pet Type Map Service. Closes #29

---
+++ pet-clinic-data/src/main/java/com/ajayonjava/aojpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.ajayonjava.aojpetclinic.services;
+
+import com.ajayonjava.aojpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/ajayonjava/aojpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.ajayonjava.aojpetclinic.services.map;
+
+import com.ajayonjava.aojpetclinic.model.PetType;
+import com.ajayonjava.aojpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
