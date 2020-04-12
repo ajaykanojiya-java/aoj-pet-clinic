f8bfee25256b395f332be599c5ed74b2dd6e6b10
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-04-12 19:23:57 +0530

    Create Pet Type with Bootstrap (Startup). Closes #31

--- pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ajayonjava/aojpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.ajayonjava.aojpetclinic.bootstrap;
 
 import com.ajayonjava.aojpetclinic.model.Owner;
+import com.ajayonjava.aojpetclinic.model.PetType;
 import com.ajayonjava.aojpetclinic.model.Vet;
 import com.ajayonjava.aojpetclinic.services.OwnerService;
+import com.ajayonjava.aojpetclinic.services.PetTypeService;
 import com.ajayonjava.aojpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,17 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     //@Autowired: no need to put @Autowired because we have defined parametrized constructor
     //so while creating object of this class spring will automatically inject the object dependency.
     //@Autowired can be used on member var of class but constructor is best practice.
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("George");
