b06ad5ba6fa9c1292797b090e0c85212b105e692
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-04-26 11:37:38 +0530

    Implements Base Entities (JPA). Closes #35

--- pet-clinic-data/src/main/java/com/ajayonjava/aojpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ajayonjava/aojpetclinic/model/BaseEntity.java
@@ -1,8 +1,18 @@
 package com.ajayonjava.aojpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+/*The JPA standard specification defines the @MappedSuperclass annotation to allow an entity to inherit properties
+ * from a base class.
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
