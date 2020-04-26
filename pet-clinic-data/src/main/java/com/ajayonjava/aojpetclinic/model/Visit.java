99a09448fbec1ce999b5584e14989187958e88b8
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-04-26 23:29:52 +0530

    Create Visit Entities. Closes #38

--- pet-clinic-data/src/main/java/com/ajayonjava/aojpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/ajayonjava/aojpetclinic/model/Visit.java
@@ -1,14 +1,20 @@
 package com.ajayonjava.aojpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "VISIT")
 public class Visit extends BaseEntity {
+
+    @Column(name = "DATE")
     private LocalDate date;
+
+    @Column(name = "DESCRIPTION")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "PET_ID")
     private Pet pet;
 
     public LocalDate getDate() {
