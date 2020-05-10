9a204d9f451e7176deda3448bffdcaa669cb8dc7
ajaykanojiya-java <kanojiyaajay22@gmail.com>
2020-05-10 08:55:45 +0530

    Upgrade to Junit 5. Closes #5

--- pet-clinic-web/src/test/java/aojpetclinic/AojPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/aojpetclinic/AojPetClinicApplicationTests.java
@@ -2,9 +2,12 @@ package aojpetclinic;
 
 import com.ajayonjava.aojpetclinic.AojPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(classes = {AojPetClinicApplication.class})
+@ExtendWith(SpringExtension.class)
 //@SpringBootTest
 class AojPetClinicApplicationTests {
 
